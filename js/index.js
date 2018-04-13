c727d6fe4b8bb6ed7a0c5a20b6a1615da718c2be
Tropic <tropical.productions.scratch@gmail.com>
2018-04-13 14:11:48 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,9 +3,13 @@ $(function() {
 });
 
 function switchSlide() {
-    $('#img-wrapper img:first').fadeOut(1000).next().fadeIn(1000, setTimeout(switchSlide2, 2000));
+    $('#img-wrapper img:first').fadeOut(1000).next().fadeIn(1000, function() {
+        setTimeout(switchSlide2, 2000)
+    });
 }
 
 function switchSlide2() {
-    $('#img-wrapper img:first').next().fadeOut(1000).prev().fadeIn(1000, setTimeout(switchSlide, 2000));
+    $('#img-wrapper img:first').next().fadeOut(1000).prev().fadeIn(1000, function() {
+        setTimeout(switchSlide, 2000)
+    });
 }
