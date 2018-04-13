fb1801d92c9d7668dc46938dadd4329472305765
Tropic <tropical.productions.scratch@gmail.com>
2018-04-13 14:21:01 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,17 +1,17 @@
 $(function() {
-    setTimeout(switchSlide, 2000);
+    setTimeout(switchSlide, 5000);
 });
 
 function switchSlide() {
     $('#img-wrapper img:first').fadeOut(1000)
     $('#img-wrapper img:first').next().fadeIn(1000, function() {
-        setTimeout(switchSlide2, 2000)
+        setTimeout(switchSlide2, 5000)
     });
 }
 
 function switchSlide2() {
     $('#img-wrapper img:first').next().fadeOut(1000)
     $('#img-wrapper img:first').fadeIn(1000, function() {
-        setTimeout(switchSlide, 2000)
+        setTimeout(switchSlide, 5000)
     });
 }
