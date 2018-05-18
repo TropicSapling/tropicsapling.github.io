bcc4ef15af16b317248432136bace4817f975815
TropicSapling <tropical.productions.scratch@gmail.com>
2018-05-18 19:06:34 +0200

    More animations & improvements

--- js/index.js
+++ js/index.js
@@ -7,9 +7,14 @@ $(function() {
     }
 	
 	$('#tropicsapling').css('visibility', 'visible');
+	$('#intro.wrapper .scroller').css('visibility', 'visible');
 	$('#tropicsapling').fadeOut(0);
+	$('#intro.wrapper .scroller').fadeOut(0);
 	setTimeout(function() {
 		$('#tropicsapling').fadeIn(2000);
+		setTimeout(function() {
+			$('#intro.wrapper .scroller').fadeIn(1000);
+		}, 2000);
 	}, 1000);
 	
 	$("#projects h1").animate({width:'toggle'}, 0);
@@ -18,9 +23,17 @@ $(function() {
 	$(".project").css('display', 'block');
 
 	$("#knowledge h1").animate({width:'toggle'}, 0);
-	$("#knowledge-grid").animate({width:'toggle'}, 0);
+	if($(window).width() < 668) {
+        // Mobile
+        $("#knowledge-grid").animate({margin: '40%'}, 0);
+    } else {
+		$("#knowledge-grid").animate({margin: '20%'}, 0);
+	}
 	$("#knowledge-grid").css('visibility', 'hidden');
-	$("#knowledge-grid").css('display', 'block');
+	
+	$("#contact.wrapper h1").animate({width:'toggle'}, 0);
+	$("#contact.wrapper > p").fadeOut(0);
+	$("#contact.wrapper div").fadeOut(0);
 	
 	setTimeout(function () {
 		$("#footer").animate({top: "95%"});
@@ -56,16 +69,27 @@ function switchSlide2() {
 	});
 }
 
-var shown = [false, false, false, false];
+var shown = [false, false, false, false, false];
 
 $(window).scroll(function(e) {
 	var scroll = $(window).scrollTop();
-	if(!shown[3] && scroll > $(window).height() * 3.75) {
+	if(!shown[4] && scroll > $(window).height() * 4.75) {
+		$("#contact.wrapper h1").animate({width:'toggle'}, 'fast', function() {
+			setTimeout(function() {
+				$("#contact.wrapper > p").fadeIn(1000);
+				setTimeout(function() {
+					$("#contact.wrapper div").fadeIn(1000);
+				}, 1500);
+			}, 500);
+		});
+		
+		shown[4] = true;
+	} else if(!shown[3] && scroll > $(window).height() * 3.75) {
 		$("#knowledge .normal-title").animate({width:'toggle'}, 'fast', function() {
-			$("#knowledge-grid").css('visibility', 'visible');
-			$("#knowledge-grid").css('display', 'none');
-
-			$("#knowledge-grid").animate({width:'toggle'}, 'fast');
+			setTimeout(function() {
+				$("#knowledge-grid").css('visibility', 'visible');
+				$("#knowledge-grid").animate({margin: 0}, 'medium');
+			}, 500);
 		});
 
 		shown[3] = true;
@@ -86,7 +110,7 @@ $(window).scroll(function(e) {
 		});
 		
 		shown[2] = true;
-	} else if(!shown[1] && scroll > $(window).height() * 2) {
+	} else if(!shown[1] && scroll > $(window).height() * 1.9) {
 		$(".project:eq(0)").css('visibility', 'visible');
 		$(".project:eq(0)").css('display', 'none');
 		
