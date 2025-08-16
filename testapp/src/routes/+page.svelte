1c01717a5481b9a59031ebe3ef85428339a2dc25
TropicSapling <tropical.productions.scratch@gmail.com>
2025-08-16 22:47:06 +0200

    Porting of first section essentially complete
    Looks good on desktop. Portrait mode needs some fixing.

--- testapp/src/routes/+page.svelte
+++ testapp/src/routes/+page.svelte
@@ -7,18 +7,18 @@
 	let test: number = 123;
 </script>
 
-<div class="relative h-full text-white">
+<div class="relative h-screen text-white">
 	<img src="img/299H.jpg" class="absolute object-cover w-full h-full -z-1" alt="Man holding sapling">
 	<div class="px-[2%] portrait:pt-[10%]">
-		<h1>TropicSapling</h1>
-		<p class="portrait:text-center">
-			<em><q class="font-bold text-2xl">Simplicity is the ultimate sophistication</q></em>
-			<br><br>
-			<span class="font-normal text-[small]">- Leonardo da Vinci</span>
+		<h1 class="text-[6em] font-['Yatra_One',_cursive] portrait:text-center portrait:text-[8em]">TropicSapling</h1>
+		<p class="font-['David_Libre',_serif] portrait:text-center">
+			<em><q class="font-bold text-[1.5em] portrait:text-[2.5em]">Simplicity is the ultimate sophistication</q></em>
+			<br>
+			<span class="font-normal text-[small] portrait:text-[large]">- Leonardo da Vinci</span>
 		</p>
 
-		<a href="" class="grid absolute left-0 w-full bottom-[10%] invert-1 animate-[jump 4s infinite]">
-			<img src="img/angle-double-down.svg" alt="scroll arrow">
+		<a href="" class="grid absolute left-0 w-full bottom-[10%] invert-100 animate-[jump_4s_infinite]">
+			<img src="img/angle-double-down.svg" alt="scroll arrow" class="justify-self-center w-[3em] portrait:w-[6em]">
 			<!-- Credit to FontAwesome, license: https://fontawesome.com/license -->
 		</a>
 	</div>
