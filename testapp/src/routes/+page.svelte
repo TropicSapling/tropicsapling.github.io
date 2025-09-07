a45f3ed4624b38eb693fe44a4801e6fab485e29f
TropicSapling <tropical.productions.scratch@gmail.com>
2025-09-07 23:29:30 +0200

    Intro stub

--- testapp/src/routes/+page.svelte
+++ testapp/src/routes/+page.svelte
@@ -7,6 +7,7 @@
 	let test: number = 123;
 </script>
 
+<!-- SECTION 1 - LANDING -->
 <div class="relative h-screen text-white">
 	<img src="img/299H.jpg" class="absolute object-cover w-full h-full -z-1" alt="Man holding sapling">
 	<div class="px-[2%] portrait:pt-[10%]">
@@ -22,4 +23,10 @@
 			<!-- Credit to FontAwesome, license: https://fontawesome.com/license -->
 		</a>
 	</div>
+</div>
+
+<!-- SECTION 2 - INTRO -->
+<div class="relative h-screen bg-[linear-gradient(#be9,_#fff)] px-[10rem] py-[4rem]">
+	<h1 class="text-center text-[5em] font-['Yatra_One',_cursive]">Hi, I'm TropicSapling</h1>
+	<p class="text-[2rem]">I'm a Swedish programmer who knows a lot of programming languages, but I'm currently mostly programming in Rust and JS. Lorem ipsum dolor sit amet, ne aperiri discere definiebas vel. Etiam audiam cu per, liber volumus nostrum at eam. Ius ea eius omnis labore, his error diceret vocibus no, ut pro vero vide commune. Minim postulant sea in. Cu quo impedit reformidans.</p>
 </div>
