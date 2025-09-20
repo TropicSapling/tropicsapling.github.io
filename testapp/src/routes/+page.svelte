d1ff4a45a3c3c23071996cde9df3d108fb1017b7
TropicSapling <tropical.productions.scratch@gmail.com>
2025-09-20 22:41:17 +0200

    Update +page.svelte

--- testapp/src/routes/+page.svelte
+++ testapp/src/routes/+page.svelte
@@ -38,11 +38,11 @@
 <!-- SECTION 2 - INTRO -->
 <div class="relative h-screen bg-[linear-gradient(#be9,_#fff)] px-[10rem] py-[4rem]">
 	<h1 class="text-center text-[5em] font-['Yatra_One',_cursive]">Hi, I'm TropicSapling</h1>
-	<p class="text-[2rem]">I'm a Swedish programmer who knows a lot of programming languages, but I'm currently mostly programming in Rust and JS. Lorem ipsum dolor sit amet, ne aperiri discere definiebas vel. Etiam audiam cu per, liber volumus nostrum at eam. Ius ea eius omnis labore, his error diceret vocibus no, ut pro vero vide commune. Minim postulant sea in. Cu quo impedit reformidans.</p>
+	<p class="text-[2rem]">I'm a Swedish programmer and engineer who loves to explore and tinker with technology. Lorem ipsum dolor sit amet, ne aperiri discere definiebas vel. Etiam audiam cu per, liber volumus nostrum at eam. Ius ea eius omnis labore, his error diceret vocibus no, ut pro vero vide commune. Minim postulant sea in. Cu quo impedit reformidans.</p>
 
 	<div>
-		<h2 class="text-center text-[5em] font-['Yatra_One',_cursive]" id="learn-more">Learn more</h2>
-		<a href="#projects" class="grid absolute left-0 w-full bottom-[10%] invert-100 animate-[jump_4s_infinite]">
+		<h2 class="text-center text-[3em] font-['Yatra_One',_cursive]">Learn more</h2>
+		<a href="#projects" class="grid absolute left-0 w-full bottom-[10%] animate-[jump_4s_infinite]">
 			<img src="img/angle-double-down.svg" alt="scroll arrow" class="justify-self-center w-[3em] portrait:w-[6em]">
 		</a>
 	</div>
