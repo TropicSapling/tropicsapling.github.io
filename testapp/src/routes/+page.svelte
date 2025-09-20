06e2b57c8eb7eca92dd6ea46df058632d92bdb70
TropicSapling <tropical.productions.scratch@gmail.com>
2025-09-20 23:11:39 +0200

    Smooth scrolling for anchors

--- testapp/src/routes/+page.svelte
+++ testapp/src/routes/+page.svelte
@@ -9,16 +9,23 @@
 	let scroll = 0;
 
 	// Always scroll the entire screen height
-	let handleScroll = e => {
+	function handleScroll(e) {
 		scroll += e.deltaY < 0 ? -screen.height : screen.height;
 		scroll  = Math.round(scroll/screen.height) * screen.height;
+
+		scrollTo({top: scroll, behavior: "smooth"})
+	}
+
+	// Smooth scrolling for anchors
+	function handleAnchor(e) {
+		scroll = document.querySelector(e.currentTarget.getAttribute("href")).offsetTop;
 
 		scrollTo({top: scroll, behavior: "smooth"})
 	}
 </script>
 
 <!-- SECTION 1 - LANDING -->
-<div class="relative h-screen text-white">
+<section class="relative h-screen text-white">
 	<img src="img/299H.jpg" class="absolute object-cover w-full h-full -z-1" alt="Man holding sapling">
 	<div class="px-[2%] portrait:pt-[10%]">
 		<h1 class="text-[6em] font-['Yatra_One',_cursive] portrait:text-center portrait:text-[8em]">TropicSapling</h1>
@@ -28,22 +35,22 @@
 			<span class="font-normal text-[small] portrait:text-[large]">- Leonardo da Vinci</span>
 		</p>
 
-		<a href="" class="grid absolute left-0 w-full bottom-[10%] invert-100 animate-[jump_4s_infinite]">
+		<a href="#intro" class="grid absolute left-0 w-full bottom-[10%] invert-100 animate-[jump_4s_infinite]" on:click|preventDefault={handleAnchor}>
 			<!-- Credit to FontAwesome, license: https://fontawesome.com/license -->
 			<img src="img/angle-double-down.svg" alt="scroll arrow" class="justify-self-center w-[3em] portrait:w-[6em]">
 		</a>
 	</div>
-</div>
+</section>
 
 <!-- SECTION 2 - INTRO -->
-<div class="relative h-screen bg-[linear-gradient(#be9,_#fff)] px-[10rem] py-[4rem]">
+<section class="relative h-screen bg-[linear-gradient(#be9,_#fff)] px-[10rem] py-[4rem]" id="intro">
 	<h1 class="text-center text-[5em] font-['Yatra_One',_cursive]">Hi, I'm TropicSapling</h1>
-	<p class="text-[2rem]">I'm a Swedish programmer and engineer who loves to explore and tinker with technology. Lorem ipsum dolor sit amet, ne aperiri discere definiebas vel. Etiam audiam cu per, liber volumus nostrum at eam. Ius ea eius omnis labore, his error diceret vocibus no, ut pro vero vide commune. Minim postulant sea in. Cu quo impedit reformidans.</p>
+	<p class="text-[2rem]">I'm a Swedish programmer and engineer who loves to explore and tinker with technology. Lorem ipsum dolor sit amet, ne aperiri discere definiebas vel. Etiam audiam cu per, liber volumus nostrum at eam. Ius ea eius omnis labore, his error diceret vocibus no, ut pro vero vide commune. Minim postulant sea in.</p>
 
 	<div>
 		<h2 class="text-center text-[3em] font-['Yatra_One',_cursive]">Learn more</h2>
-		<a href="#projects" class="grid absolute left-0 w-full bottom-[10%] animate-[jump_4s_infinite]">
-			<img src="img/angle-double-down.svg" alt="scroll arrow" class="justify-self-center w-[3em] portrait:w-[6em]">
+		<a href="#projects" class="grid absolute left-0 w-full bottom-[10%] animate-[jump_4s_infinite]" on:click|preventDefault={handleAnchor}>
+			<img src="img/angle-double-down.svg" alt="scroll arrow" class="justify-self-center w-[2em] portrait:w-[4em]">
 		</a>
 	</div>
-</div>
+</section>
