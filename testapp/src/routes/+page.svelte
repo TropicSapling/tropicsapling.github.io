1d45bcf577a8ef41df1b8ac16eae1b8b448ea0c1
TropicSapling <tropical.productions.scratch@gmail.com>
2025-10-04 19:37:01 +0200

    Improved consistency, responsiveness

--- testapp/src/routes/+page.svelte
+++ testapp/src/routes/+page.svelte
@@ -27,37 +27,37 @@
 <!-- SECTION 1 - LANDING -->
 <section class="relative h-screen text-white">
 	<img src="img/299H.jpg" class="absolute object-cover w-full h-full -z-1" alt="Man holding sapling">
-	<div class="px-[2%] portrait:pt-[10%]">
-		<h1 class="text-[6em] font-['Yatra_One',_cursive] portrait:text-center portrait:text-[2.5em]">TropicSapling</h1>
-		<p class="font-['David_Libre',_serif] portrait:text-center">
-			<em><q class="font-bold text-[1.5em] portrait:text-[1em]">Simplicity is the ultimate sophistication</q></em>
+	<div class="px-[2%] pt-[10%] landscape:pt-0">
+		<h1 class="text-[2.5em] landscape:text-[6em] font-['Yatra_One',_cursive] text-center landscape:text-left">TropicSapling</h1>
+		<p class="font-['David_Libre',_serif] text-center landscape:text-left">
+			<em><q class="font-bold text-[1em] landscape:text-[1.5em]">Simplicity is the ultimate sophistication</q></em>
 			<br>
 			<span class="font-normal text-[small]">- Leonardo da Vinci</span>
 		</p>
 
 		<a href="#intro" class="grid absolute left-0 w-full bottom-[10%] invert-100 animate-[jump_4s_infinite]" on:click|preventDefault={handleAnchor}>
 			<!-- Credit to FontAwesome, license: https://fontawesome.com/license -->
-			<img src="img/angle-double-down.svg" alt="scroll arrow" class="justify-self-center w-[3em] portrait:w-[2em]">
+			<img src="img/angle-double-down.svg" alt="scroll arrow" class="justify-self-center w-[2em] landscape:w-[3em]">
 		</a>
 	</div>
 </section>
 
 <!-- SECTION 2 - INTRO -->
-<section class="grid gap-20 relative h-screen bg-[linear-gradient(#be9,_#fff)] px-[10rem] py-[4rem] portrait:px-[2rem] portrait:py-[2rem]" id="intro">
+<section class="grid gap-20 relative h-screen bg-[linear-gradient(#be9,_#fff)] px-[2rem] landscape:px-[10rem] py-[2rem] landscape:py-[4rem]" id="intro">
 	<div>
-		<h1 class="text-center text-[5em] font-['Yatra_One',_cursive] portrait:text-[2.5em]">Hi, I'm TropicSapling</h1>
-		<p class="text-[2rem] portrait:text-[1rem] portrait:text-center">I'm a Swedish programmer and engineer who loves to explore and tinker with technology. Lorem ipsum dolor sit amet, ne aperiri discere definiebas vel. Etiam audiam cu per, liber volumus nostrum at eam. Ius ea eius omnis labore, his error diceret vocibus no, ut pro vero vide commune. Minim postulant sea in.</p>
+		<h1 class="text-center text-[2.5em] landscape:text-[5em] font-['Yatra_One',_cursive]">Hi, I'm TropicSapling</h1>
+		<p class="text-[1rem] landscape:text-[2rem] text-center landscape:text-left">I'm a Swedish programmer and engineer who loves to explore and tinker with technology. Lorem ipsum dolor sit amet, ne aperiri discere definiebas vel. Etiam audiam cu per, liber volumus nostrum at eam. Ius ea eius omnis labore, his error diceret vocibus no, ut pro vero vide commune. Minim postulant sea in.</p>
 	</div>
 
 	<div>
-		<h2 class="text-center text-[3em] font-['Yatra_One',_cursive] portrait:text-[2em]">Learn more</h2>
+		<h2 class="text-center text-[2em] landscape:text-[3em] font-['Yatra_One',_cursive]">Learn more</h2>
 		<a href="#projects" class="grid left-0 w-full animate-[jump_4s_infinite]" on:click|preventDefault={handleAnchor}>
-			<img src="img/angle-double-down.svg" alt="scroll arrow" class="justify-self-center w-[2em] portrait:w-[1.5em]">
+			<img src="img/angle-double-down.svg" alt="scroll arrow" class="justify-self-center w-[1.5em] landscape:w-[2em]">
 		</a>
 	</div>
 </section>
 
 <!-- SECTION 3 - PROJECTS -->
-<section class="relative h-screen" id="projects">
-	<h1 class="text-center text-[5em] font-['Yatra_One',_cursive] portrait:text-[2.5em]">Projects</h1>
+<section class="relative h-screen py-[2rem] landscape:py-[4rem]" id="projects">
+	<h1 class="text-center text-[2.5em] landscape:text-[5em] font-['Yatra_One',_cursive]">Projects</h1>
 </section>
