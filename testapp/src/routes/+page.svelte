0fc162b5925184627345f6bd36bc81a926489fb4
TropicSapling <tropical.productions.scratch@gmail.com>
2025-10-13 22:30:40 +0200

    More idiomatic tailwind

--- testapp/src/routes/+page.svelte
+++ testapp/src/routes/+page.svelte
@@ -27,37 +27,37 @@
 <!-- SECTION 1 - LANDING -->
 <section class="relative h-screen text-white">
 	<img src="img/299H.jpg" class="absolute object-cover w-full h-full -z-1" alt="Man holding sapling">
-	<div class="px-[2%] pt-[10%] landscape:pt-0">
-		<h1 class="text-[2.5em] landscape:text-[6em] font-['Yatra_One',_cursive] text-center landscape:text-left">TropicSapling</h1>
+	<div class="px-5 pt-8 landscape:pt-2.5">
+		<h1 class="text-4xl landscape:text-8xl font-['Yatra_One',_cursive] text-center landscape:text-left">TropicSapling</h1>
 		<p class="font-['David_Libre',_serif] text-center landscape:text-left">
-			<em><q class="font-bold text-[1em] landscape:text-[1.5em]">Simplicity is the ultimate sophistication</q></em>
+			<em><q class="font-bold landscape:text-2xl">Simplicity is the ultimate sophistication</q></em>
 			<br>
-			<span class="font-normal text-[small]">- Leonardo da Vinci</span>
+			<span class="font-normal text-xs">- Leonardo da Vinci</span>
 		</p>
 
-		<a href="#intro" class="grid absolute left-0 w-full bottom-[10%] invert-100 animate-[jump_4s_infinite]" on:click|preventDefault={handleAnchor}>
+		<a href="#intro" class="grid absolute left-0 w-full bottom-1/10 invert-100 animate-[jump_4s_infinite]" on:click|preventDefault={handleAnchor}>
 			<!-- Credit to FontAwesome, license: https://fontawesome.com/license -->
-			<img src="img/angle-double-down.svg" alt="scroll arrow" class="justify-self-center w-[2em] landscape:w-[3em]">
+			<img src="img/angle-double-down.svg" alt="scroll arrow" class="justify-self-center w-8 landscape:w-12">
 		</a>
 	</div>
 </section>
 
 <!-- SECTION 2 - INTRO -->
-<section class="grid gap-20 relative h-screen bg-[linear-gradient(#be9,_#fff)] px-[2rem] landscape:px-[10rem] py-[2rem] landscape:py-[4rem]" id="intro">
+<section class="grid gap-20 relative h-screen bg-[linear-gradient(#be9,_#fff)] p-8 landscape:px-40 landscape:py-16" id="intro">
 	<div>
-		<h1 class="text-center text-[2.5em] landscape:text-[5em] font-['Yatra_One',_cursive]">Hi, I'm TropicSapling</h1>
-		<p class="text-[1rem] landscape:text-[2rem] text-center landscape:text-left">I'm a Swedish programmer and engineer who loves to explore and tinker with technology. Lorem ipsum dolor sit amet, ne aperiri discere definiebas vel. Etiam audiam cu per, liber volumus nostrum at eam. Ius ea eius omnis labore, his error diceret vocibus no, ut pro vero vide commune. Minim postulant sea in.</p>
+		<h1 class="text-center text-4xl landscape:text-7xl font-['Yatra_One',_cursive]">Hi, I'm TropicSapling</h1>
+		<p class="landscape:text-3xl text-center landscape:text-left">I'm a Swedish programmer and engineer who loves to explore and tinker with technology. Lorem ipsum dolor sit amet, ne aperiri discere definiebas vel. Etiam audiam cu per, liber volumus nostrum at eam. Ius ea eius omnis labore, his error diceret vocibus no, ut pro vero vide commune. Minim postulant sea in.</p>
 	</div>
 
 	<div>
-		<h2 class="text-center text-[2em] landscape:text-[3em] font-['Yatra_One',_cursive]">Learn more</h2>
+		<h2 class="text-center text-3xl landscape:text-5xl font-['Yatra_One',_cursive]">Learn more</h2>
 		<a href="#projects" class="grid left-0 w-full animate-[jump_4s_infinite]" on:click|preventDefault={handleAnchor}>
-			<img src="img/angle-double-down.svg" alt="scroll arrow" class="justify-self-center w-[1.5em] landscape:w-[2em]">
+			<img src="img/angle-double-down.svg" alt="scroll arrow" class="justify-self-center w-6 landscape:w-8">
 		</a>
 	</div>
 </section>
 
 <!-- SECTION 3 - PROJECTS -->
-<section class="relative h-screen py-[2rem] landscape:py-[4rem]" id="projects">
-	<h1 class="text-center text-[2.5em] landscape:text-[5em] font-['Yatra_One',_cursive]">Projects</h1>
+<section class="relative h-screen p-8 landscape:px-40 landscape:py-16" id="projects">
+	<h1 class="text-center text-4xl landscape:text-7xl font-['Yatra_One',_cursive]">Projects</h1>
 </section>
