67628bb2ecb5da333e912b2df603e8be6d25fee5
TropicSapling <tropical.productions.scratch@gmail.com>
2025-10-26 17:30:28 +0100

    Skill list changes

--- testapp/src/routes/+page.svelte
+++ testapp/src/routes/+page.svelte
@@ -30,9 +30,9 @@
 	const skills = [
 		{name:'Front-end / UI', desc:'JavaScript, HTML/CSS, Svelte, Tailwind, Figma', c:'bg-emerald-400'},
 		{name:'Back-end / Systems', desc:'Rust, C/C++, Python, Go, Java, SQL, Node.js, Bash', c:'bg-sky-400'},
-		{name:'Collaboration Tools', desc:'Google Workspace, Google Colab, GitHub, Trello', fill:86, c:'bg-yellow-400'},
-		{name:'Artificial Intelligence', desc:'Pytorch, Keras, scikit-learn, GitHub Copilot', c:'bg-rose-400'},
-		{name:'Cloud / Package Management', desc:'Docker, Cargo, git, npm, pip, apt', c:'bg-indigo-400'},
+		{name:'Collaboration Tools', desc:'Google Workspace, Google Colab, GitHub, Git, Trello', fill:86, c:'bg-yellow-400'},
+		{name:'Artificial Intelligence', desc:'Keras, scikit-learn, Pytorch, AI-assisted development', c:'bg-rose-400'},
+		{name:'Cloud Development', desc:'Docker, Cargo, npm, pip, apt', c:'bg-indigo-400'},
 		{name:'Development Environments', desc:'Sublime Text, Visual Studio Code, Android Studio', c:'bg-teal-400'}
 	];
 
