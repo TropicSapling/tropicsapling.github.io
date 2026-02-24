24198f28ba5c446d9fcc45abb8ebf53122ef0fee
TropicSapling <tropical.productions.scratch@gmail.com>
2026-02-24 21:23:04 +0100

    Link fix

--- src/routes/consts.ts
+++ src/routes/consts.ts
@@ -1,7 +1,7 @@
 export const projects = [
 	{
 		name: 'AI Evolution Simulation',
-		link: 'neural-network-evolution',
+		link: 'https://tropicsapling.github.io/neural-network-evolution',
 		imag: 'bg-[url(img/moving-ai.PNG)]'
 	},
 	{
